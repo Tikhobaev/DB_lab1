ddaf02a700be770f94002c1a3836880302bccf95
Ilya Tikhobaev <dontixabon@gmail.com>
2020-03-07 18:18:28 +0300

    Fix bugs in search, add, insert functions, add menu

--- database.py
+++ database.py
@@ -5,28 +5,34 @@ import openpyxl
 
 
 class FilmsDatabase:
-    def __init__(self, *args):
-        if os.path.exists(f'Data/{args[0]}'):
-            self.filename = args[0]
-            rows = []
-            with open(f'Data/{args[0]}', 'r+') as data_file:
-                for line in data_file:
-                    raw_row = line.split(';')
-                    rows.append([raw_row[0].replace('^', ''), int(raw_row[1]), float(raw_row[2]), int(raw_row[3])])
-            if rows:
-                self.db = rows
-                self.id_dicts = dict([[rows[i][3], i] for i in range(0, len(rows))])
-            else:
-                self.db = []
-                self.id_dicts = {}
-        else:
-            self.name = args[0]
+    def __init__(self, filename, empty=False):
+        if empty:
+            self.filename = filename
             self.db = []
             self.id_dicts = {}
-            if self.name.endswith(r'.hdb'):
-                self.filename = self.name
+            self.name = ''
+        else:
+            if os.path.exists(filename):
+                self.filename = filename
+                rows = []
+                with open(filename, 'r') as data_file:
+                    for line in data_file:
+                        raw_row = line.split(';')
+                        rows.append([raw_row[0].replace('^', ''), int(raw_row[1]), float(raw_row[2]), int(raw_row[3])])
+                if rows:
+                    self.db = rows
+                    self.id_dicts = dict([[rows[i][3], i] for i in range(0, len(rows))])
+                else:
+                    self.db = []
+                    self.id_dicts = {}
             else:
-                self.filename = f'{self.name}.hdb'
+                self.name = filename
+                self.db = []
+                self.id_dicts = {}
+                if self.name.endswith(r'.hdb'):
+                    self.filename = self.name
+                else:
+                    self.filename = f'{self.name}.hdb'
 
         self.node_size_in_bytes = 48
         self.field_lengths = [30, 4, 3, 7]
@@ -37,69 +43,64 @@ class FilmsDatabase:
         self.id_dicts = new_db.id_dicts
 
     def save_db(self, filename):
-        with open(filename, 'w') as data_file:
-            counter = 0
-            for node in self.db:
-                node[0] = node[0] + (self.field_lengths[0] - len(node[0])) * '^'
-                node[1] = str(node[1])
-                node[2] = (self.field_lengths[2] - len(str(node[2]))) * '0' + str(node[2])
-                node[3] = str(node[3])
-                counter += 1
-                data_file.write(';'.join(node))
-                if counter < len(self.db):
-                    data_file.write('\n')
+        if filename:
+            with open(filename, 'w') as data_file:
+                counter = 0
+                for node in self.db:
+                    node[0] = node[0] + (self.field_lengths[0] - len(node[0])) * '^'
+                    node[1] = str(node[1])
+                    node[2] = (self.field_lengths[2] - len(str(node[2]))) * '0' + str(node[2])
+                    node[3] = str(node[3])
+                    counter += 1
+                    data_file.write(';'.join(node))
+                    if counter < len(self.db):
+                        data_file.write('\n')
 
     @staticmethod
     def create_db(filename):
-        with open(f'Data/{filename}', 'w') as f:
+        with open(filename, 'w') as f:
             pass
         return True
 
     @staticmethod
     def open_db(filename):
-        if os.path.exists(f'Data/{filename}'):
+        if os.path.exists(filename):
             return FilmsDatabase(filename)
         return None
 
     @staticmethod
     def del_db(filename):
-        if os.path.exists(f'Data/{filename}'):
+        if os.path.exists(filename):
             os.remove(filename)
             return True
         return None
 
-    @staticmethod
-    def make_backup(filename):
-        try:
-            os.mkdir('Backup')
-
-        except FileExistsError:
-            pass
-        os.chdir('Backup')
-        newzip = zipfile.ZipFile(f'{filename}.zip', 'w')
-        os.chdir('../Data')
-        newzip.write(filename)
-        os.chdir('..')
-        newzip.close()
+    def make_backup(self, filename):
+        if self.filename:
+            newzip = zipfile.ZipFile(filename, 'w')
+            newzip.write(self.filename)
+            newzip.close()
 
     @staticmethod
-    def from_backup(filename):
-        arch = zipfile.ZipFile(f'Backup/{filename}.zip', 'r')
-        arch.extract(filename, f'Data/{filename}_extracted')
-        os.replace(f'Data/{filename}_extracted/{filename}', f'Data/{filename}')
-        os.rmdir(f'Data/{filename}_extracted')
+    def from_backup(filename_zip, final_dir):
+        arch = zipfile.ZipFile(filename_zip, 'r')
+        filename = filename_zip.split('/')[-1].replace('.zip', '')
+        arch.extract(filename, 'extracted')
         arch.close()
+        os.replace(f'extracted/{filename}', final_dir + filename)
+        os.rmdir(f'extracted')
 
-    def save_to_xlsx(self):
+    def save_to_xlsx(self, filename):
         wb = openpyxl.Workbook()
-        wb.create_sheet(title=self.filename, index=0)
-        sheet = wb[self.filename]
+        wb_name = filename.split('/')[-1].replace('.xlsx', '')
+        wb.create_sheet(title=wb_name, index=0)
+        sheet = wb[wb_name]
         for i in range(0, len(self.db)):
             for j in range(0, 4):
                 value = self.db[i][j]
                 cell = sheet.cell(row=i+1, column=j+1)
                 cell.value = value
-        wb.save(f'{self.filename.replace(r".hdb", "")}.xlsx')
+        wb.save(filename)
 
     def search_by_name(self, name) -> list:
         result = []
@@ -128,16 +129,17 @@ class FilmsDatabase:
         node_id = int(node_id)
         result = []
         if self.id_dicts.get(node_id) is not None:
-            result = self.db[self.id_dicts[node_id]]
+            result.append(self.db[self.id_dicts[node_id]])
         return result
 
     def add(self, new_node: list):
-        # TODO make logging
         # if id value already exists
         if self.id_dicts.get(int(new_node[3])) is not None:
             return
         try:
-            with open('Data/' + self.filename, 'a') as data_file:
+            with open(self.filename, 'a') as data_file:
+                self.db.append([new_node[0], int(new_node[1]), float(new_node[2]), int(new_node[3])])
+                self.id_dicts[new_node[3]] = len(self.db) - 1
                 if len(new_node[0]) < self.field_lengths[0]:
                     new_node[0] = new_node[0] + '^' * (self.field_lengths[0] - len(new_node[0]))
                 if len(new_node[2]) != self.field_lengths[2]:
@@ -150,17 +152,15 @@ class FilmsDatabase:
                     data_to_insert = ';'.join(new_node)
                     if len(data_to_insert) == self.node_size_in_bytes - 1:
                         data_file.write(data_to_insert)
-                self.db.append([new_node[0], int(new_node[1]), float(new_node[2]), int(new_node[3])])
-                self.id_dicts[new_node[3]] = len(self.db) - 1
         except Exception:
-            print('Incorrect data:')
+            pass
 
     def delete_node(self, node_id):
         node_id = int(node_id)
         if self.id_dicts.get(node_id) is None:
             print('This id is not exists')
             return
-        with open('Data/' + self.filename, 'r+') as data_file:
+        with open(self.filename, 'r+') as data_file:
             data_file.seek(self.node_size_in_bytes * (len(self.db) - 1))
             final_node = data_file.readline().split(';')
             final_node_id = int(final_node[3])
@@ -170,9 +170,11 @@ class FilmsDatabase:
                 data_file.truncate(self.node_size_in_bytes * (len(self.db) - 1) - 1)
             else:
                 data_file.truncate(self.node_size_in_bytes * (len(self.db) - 1))
-        del self.db[self.id_dicts[final_node_id]]
+        self.db[self.id_dicts[node_id]] = self.db[self.id_dicts[final_node_id]]
+        self.db.pop()
         self.id_dicts[final_node_id] = self.id_dicts[node_id]
         del self.id_dicts[node_id]
+        pass
 
     def change_node(self, updated_data: list):
         """
@@ -188,7 +190,7 @@ class FilmsDatabase:
             updated_data[0] += '^' * (self.field_lengths[0] - len(updated_data[0]))
         if len(updated_data[2]) != self.field_lengths[2]:
             updated_data[2] = '0' * (self.field_lengths[2] - len(updated_data[2])) + updated_data[2]
-        with open('Data/' + self.filename, 'r+') as data_file:
+        with open(self.filename, 'r+') as data_file:
             data_file.seek(self.node_size_in_bytes * self.id_dicts[node_id])
             data_file.write(';'.join(updated_data))
         return True
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+
 from database import FilmsDatabase
 from tkinter import *
-from tkinter import messagebox
+from tkinter import filedialog as fd
 
 NUMBER_OF_FIELDS = 4
 SEARCH_BY_NAME = 0
@@ -10,19 +12,19 @@ SEARCH_BY_ID = 3
 
 
 def add_buttons(root, entries, main_database, listboxes, search_type):
-    btn_create_db = Button(root, text="Create database",
-                           command=lambda: global_action(FilmsDatabase.create_db, 'Create database', main_database))
+    '''btn_create_db = Button(root, text="Create database",
+                           command=lambda: save_as(FilmsDatabase.create_db, 'Create database', main_database))
     btn_delete_db = Button(root, text="Delete database",
                            command=lambda: global_action(FilmsDatabase.del_db, 'Delete database', main_database))
     btn_open_db = Button(root, text="Open database",
-                         command=lambda: global_action(FilmsDatabase.open_db, 'Open database', main_database))
+                         command= lambda: open_db(FilmsDatabase.open_db, main_database))
     btn_save_db = Button(root, text="Save current database",
                          command=lambda: save_main_db(main_database))  # нажатие
 
     btn_create_db.place(relheight=0.05, relwidth=0.33)
     btn_delete_db.place(relheight=0.05, relwidth=0.33, relx=0.33)
     btn_open_db.place(relheight=0.05, relwidth=0.34, relx=0.66)
-    btn_save_db.place(relheight=0.05, relwidth=0.25, relx=0.5, rely=0.92)
+    btn_save_db.place(relheight=0.05, relwidth=0.25, relx=0.5, rely=0.92)'''
 
     btn_name_search = Button(root, text="Search",
                              command=lambda: print_data(search(search_type.get(), entries, main_database), listboxes))
@@ -32,29 +34,33 @@ def add_buttons(root, entries, main_database, listboxes, search_type):
                       command=lambda: drop(search_type.get(), main_database, entries))
     btn_change = Button(root, text='Change',
                         command=lambda: change(main_database, entries))
-    btn_make_backup = Button(root, text='Make backup file',
+    '''btn_make_backup = Button(root, text='Make backup file',
                              command=lambda: global_action(FilmsDatabase.make_backup, 'Make backup', main_database))
     btn_from_backup = Button(root, text='Extract from backup',
-                             command=lambda: global_action(FilmsDatabase.from_backup, 'Extract DB', main_database))
+                             command=lambda: global_action(FilmsDatabase.from_backup, 'Extract DB', main_database))'''
+    btn_save_excel = Button(root, text='Save to .xlsx',
+                            command=main_database.save_to_xlsx)
     btn_name_search.place(relheight=0.05, relwidth=0.25, rely=0.05)
     btn_insert.place(relheight=0.05, relwidth=0.25, relx=0.25, rely=0.05)
     btn_drop.place(relheight=0.05, relwidth=0.25, relx=0.5, rely=0.05)
     btn_change.place(relheight=0.05, relwidth=0.25, relx=0.75, rely=0.05)
-    btn_make_backup.place(relheight=0.05, relwidth=0.2, relx=0.05, rely=0.92)
-    btn_from_backup.place(relheight=0.05, relwidth=0.2, relx=0.25, rely=0.92)
+    # btn_make_backup.place(relheight=0.05, relwidth=0.15, rely=0.92)
+    # btn_from_backup.place(relheight=0.05, relwidth=0.15, relx=0.16, rely=0.92)
+    btn_save_excel.place(relheight=0.05, relwidth=0.15, relx=0.32, rely=0.92)
 
 
 def drop(mode, main_database, entries):
     search_result = search(mode, entries, main_database)
     for node in search_result:
         main_database.delete_node(node[3])
+    pass
 
 
 def change(main_database, entries):
     if not main_database.change_node([entries[0].get(),
-                               entries[1].get(),
-                               entries[2].get(),
-                               entries[3].get()]):
+                                      entries[1].get(),
+                                      entries[2].get(),
+                                      entries[3].get()]):
         error_window = Toplevel()
         error_window.geometry('300x150+750+450')
         error_window.title('Error')
@@ -62,7 +68,6 @@ def change(main_database, entries):
         label_error.place(relheight=0.25, relwidth=1, rely=0.08)
         btn_ok = Button(error_window, text='Ok', command=lambda: error_window.destroy())
         btn_ok.place(relheight=0.3, relwidth=0.4, relx=0.3, rely=0.45)
-
 
 
 def add_labels(root):
@@ -111,37 +116,101 @@ def add_scroll_listboxes(root):
     return listboxes
 
 
-def add_menu(root):
-    mb = Menubutton(root, text="Search by...", relief=RAISED)
-    mb.place(relheight=0.05, relwidth=0.2, relx=0.8, rely=0.92)
-    mb.menu = Menu(mb, tearoff=0)
-    mb["menu"] = mb.menu
+def add_menu(root, main_database):
+    mainmenu = Menu(root)
+    root.config(menu=mainmenu)
 
+    filemenu = Menu(mainmenu, tearoff=0)
+    filemenu.add_command(label="Open", command=lambda: open_db(main_database))
+    filemenu.add_command(label="New", command=lambda: create_db(main_database))
+    filemenu.add_command(label="Save", command=lambda: main_database.save_db(main_database.filename))
+    filemenu.add_command(label="Save as", command=lambda: save_as(main_database))
+    filemenu.add_command(label="Save as excel", command=lambda: save_excel(main_database))
+
+    backup_menu = Menu(mainmenu, tearoff=0)
+    backup_menu.add_command(label="Make backup file", command=lambda: make_backup(main_database))
+    backup_menu.add_command(label="Open from backup...", command=from_backup)
+
+    select_field_menu = Menu(mainmenu, tearoff=0)
     search_type_var = IntVar()
+    select_field_menu.add_radiobutton(label="name", value=0,
+                            variable=search_type_var)
+    select_field_menu.add_radiobutton(label="year", value=1,
+                            variable=search_type_var)
+    select_field_menu.add_radiobutton(label="rate", value=2,
+                            variable=search_type_var)
+    select_field_menu.add_radiobutton(label="id", value=3,
+                            variable=search_type_var)
 
-    mb.menu.add_radiobutton(label="name", value=0,
-                            variable=search_type_var)
-    mb.menu.add_radiobutton(label="year", value=1,
-                            variable=search_type_var)
-    mb.menu.add_radiobutton(label="rate", value=2,
-                            variable=search_type_var)
-    mb.menu.add_radiobutton(label="id", value=3,
-                            variable=search_type_var)
+    mainmenu.add_cascade(label="File", menu=filemenu)
+    mainmenu.add_cascade(label="Backup", menu=backup_menu)
+    mainmenu.add_cascade(label='Select field', menu=select_field_menu)
+
+
+    # select_field_menu.add_command(label='Select field', command=lambda: )
+
+
+    #mb = Menubutton(mainmenu, text="Search by...", relief=RAISED)
+    #mb.place(relheight=0.05, relwidth=0.2, relx=0.8, rely=0.92)
+    #mb.menu = Menu(mb, tearoff=0)
+    #mb["menu"] = mb.menu
+
     return search_type_var
 
 
-def save_main_db(main_database):
-    main_database.save_db(main_database.filename)
-
-
-def opener(main_database, new_database):
-    if main_database:
+def open_db(main_database):
+    file_name = fd.askopenfilename()
+    new_db = FilmsDatabase(file_name)
+    if new_db:
         main_database.save_db(main_database.filename)
-    main_database.change(new_database)
+        main_database.change(new_db)
 
 
-def global_action(func, text, main_database):
+def create_db(main_database):
+    os.chdir('Data')
+    file_name = fd.asksaveasfilename(filetypes=(("HDB files", "*.hdb"),
+                                                ("All files", "*.*")))
+    os.chdir('..')
+    new_db = FilmsDatabase(file_name)
+    main_database.save_db(main_database.filename)
+    main_database.change(new_db)
+
+
+def save_as(main_database):
+    os.chdir('Data')
+    file_name = fd.asksaveasfilename(filetypes=(("HDB files", "*.hdb"),
+                                                ("All files", "*.*")))
+    os.chdir('..')
+    main_database.save_db(file_name)
+
+
+def make_backup(main_database):
+    os.chdir('Backup')
+    file_name = fd.asksaveasfilename(filetypes=(("ZIP files", "*.zip"),
+                                                ("All files", "*.*")))
+    os.chdir('..')
+    main_database.make_backup(file_name)
+
+
+def from_backup():
+    file_name_zip = fd.askopenfilename(initialdir=os.getcwd() + '/Backup',
+                                       title='Select file to unzip',
+                                       filetypes=(("ZIP files", "*.zip"),
+                                                  ("All files", "*.*")))
+    FilmsDatabase.from_backup(file_name_zip, os.getcwd() + '/Data/')
+
+
+def save_excel(main_database):
+    os.chdir('Data')
+    file_name = fd.asksaveasfilename(filetypes=(("XLSX files", "*.xlsx"),
+                                                ("All files", "*.*")))
+    os.chdir('..')
+    main_database.save_to_xlsx(file_name)
+
+
+'''def global_action(func, text, main_database):
     def command_action(main_database):
+
         if filename.get():
             returned_value = func(filename.get())
             if returned_value:
@@ -166,7 +235,7 @@ def global_action(func, text, main_database):
     entry_filename = Entry(create_window, textvariable=filename, font=14)
     entry_filename.place(relheight=0.2, relwidth=0.8, relx=0.1, rely=0.3)
     btn_create = Button(create_window, text=text, command=lambda: command_action(main_database))
-    btn_create.place(relheight=0.2, relwidth=0.5, relx=0.25, rely=0.7)
+    btn_create.place(relheight=0.2, relwidth=0.5, relx=0.25, rely=0.7)'''
 
 
 def search(mode, entries, main_database):
@@ -212,13 +281,43 @@ def delete():
     pass
 
 
+def start_create_db(window, main_database):
+    os.chdir('Data')
+    file_name = fd.asksaveasfilename(filetypes=(("HDB files", "*.hdb"),
+                                                ("All files", "*.*")))
+    os.chdir('..')
+    new_db = FilmsDatabase(file_name, empty=True)
+    main_database.change(new_db)
+    window.destroy()
+
+
+def start_open_db(window, main_database):
+    os.chdir('Data')
+    file_name = fd.askopenfilename()
+    os.chdir('..')
+    opened_db = FilmsDatabase.open_db(file_name)
+    main_database.change(opened_db)
+    window.destroy()
+
+
 def main():
+    main_database = FilmsDatabase('', empty=True)
+    start_window = Tk()
+    start_window.title('Open or create?')
+    start_window.geometry('300x150+700+400')
+    filename = StringVar()
+    label_filename = Label(start_window, text='Select option:', font=14)
+    label_filename.place(relheight=0.3, relwidth=0.8, relx=0.1)
+    btn_create = Button(start_window, text='Create database',
+                        command=lambda: start_create_db(start_window, main_database))
+    btn_create.place(relheight=0.25, relwidth=0.5, relx=0.25, rely=0.7)
+    btn_open = Button(start_window, text='Open database', command=lambda: start_open_db(start_window, main_database))
+    btn_open.place(relheight=0.25, relwidth=0.5, relx=0.25, rely=0.3)
+    start_window.mainloop()
+
     root = Tk()
     root.title('Interactive database')
     root.geometry('900x700+350+70')
-
-    main_database = FilmsDatabase('config.hdb')
-
     name_input = StringVar()
     year_input = StringVar()
     rate_input = StringVar()
@@ -226,7 +325,7 @@ def main():
     entries = add_entries(root, name_input, year_input, rate_input, id_input)
     add_labels(root)
     listboxes = add_scroll_listboxes(root)
-    search_type = add_menu(root)
+    search_type = add_menu(root, main_database)
     add_buttons(root, entries, main_database, listboxes, search_type)
 
     root.mainloop()
